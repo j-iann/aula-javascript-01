b3b9df245b4bae7fcafcf104d59dee6e5c73db8a
ian <jesperiansantos.2021@gmail.com>
2021-09-24 07:15:54 -0300

    enviando arquivos

---
+++ entrega1.js
@@ -0,0 +1,6 @@
+var endereco = "Rua Blá Blá Blá, 001"
+var nutrientes = {gordura: "transaturada", vitamina: "d"}
+let nome_biscoito = "Renata"
+let idade = 30
+const pi = 3.14
+const diametro = r**2
